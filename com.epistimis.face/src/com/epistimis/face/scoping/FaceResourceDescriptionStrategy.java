b0e0de765edb55da369ed3b148b4fd0f8c3ca497
Steve Hickman <steve@epistimis.com>
2022-12-30 12:25:59 -0800

    Draft ResourceDescriptionStrategy
    Not yet enabled - update FaceRuntimeModule to enable - and check to see
    what it should actually do.

---
+++ com.epistimis.face/src/com/epistimis/face/scoping/FaceResourceDescriptionStrategy.java
@@ -0,0 +1,28 @@
+package com.epistimis.face.scoping;
+
+import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
+import org.eclipse.xtext.util.IAcceptor;
+
+import com.epistimis.face.face.IntegrationTSNodeConnection;
+import com.google.inject.Singleton;
+
+public class FaceResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
+
+	public FaceResourceDescriptionStrategy() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override 
+	public boolean createEObjectDescriptions(EObject eObject, IAcceptor<IEObjectDescription> acceptor) {
+		if ((eObject instanceof IntegrationTSNodeConnection) 
+				)
+		{
+			// don't index contents of a block
+			return false;
+		} else
+			return super.createEObjectDescriptions(eObject, acceptor);
+	}
+
+}
