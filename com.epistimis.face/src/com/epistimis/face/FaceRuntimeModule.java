02b4936f92fbd8a4ba90d82a8233c705c9e49ca3
Steve Hickman <steve@epistimis.com>
2023-07-18 19:47:06 -0700

    Draft changes to add 'include' functionality
    This needs to be uncommented to activate. Corresponding commits in Uddl,
    Privacy and Omodel.

--- com.epistimis.face/src/com/epistimis/face/FaceRuntimeModule.java
+++ com.epistimis.face/src/com/epistimis/face/FaceRuntimeModule.java
@@ -5,8 +5,12 @@ package com.epistimis.face;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
+import org.eclipse.xtext.scoping.IGlobalScopeProvider;
 
 import com.epistimis.face.generator.QueryUtilities;
+import com.epistimis.face.scoping.FaceGlobalScopeProvider;
+import com.epistimis.face.scoping.FaceResourceDescriptionsStrategy;
 
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
@@ -46,8 +50,18 @@ public class FaceRuntimeModule extends AbstractFaceRuntimeModule {
 	/** Enable this if there are performance issues with name resolution. And then look at the strategy to see what should
 	 * be excluded from the index
 	 * */
-//	public  Class<? extends IDefaultResourceDescriptionStrategy> bindIDefaultResourceDescriptionStrategy() {
-//		return FaceResourceDescriptionsStrategy.class;
+	public  Class<? extends IDefaultResourceDescriptionStrategy> bindIDefaultResourceDescriptionStrategy() {
+		return FaceResourceDescriptionsStrategy.class;
+	}
+
+
+//	// Enable imports by uncommenting this. The default is to import anything visible in a project
+//	// See section 3.3.1,3.3.2 of the Advanced XText Manual PDF 
+//	// or https://blogs.itemis.com/en/in-five-minutes-to-transitive-imports-within-a-dsl-with-xtext
+//	@Override
+//	public
+//	Class<? extends IGlobalScopeProvider> bindIGlobalScopeProvider() {
+//		return FaceGlobalScopeProvider.class;
 //	}
 
 }
