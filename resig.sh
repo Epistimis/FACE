7d31670aa33ca3b80e2cf95f18617da6dc79dbec
Steve Hickman <steve@epistimis.com>
2023-11-30 16:41:18 -0800

    Don't track generated UI test files

--- resig.sh
+++ resig.sh
@@ -3,7 +3,8 @@ git restore com.epistimis.face.ide/src-gen/.gitignore
 git restore com.epistimis.face.ide/xtend-gen/.gitignore
 git restore com.epistimis.face.tests/src-gen/.gitignore
 git restore com.epistimis.face/tests/xtend-gen/.gitignore
-git restore  com.epistimis.face.ui.tests/xtend-gen/.gitignore
+git restore com.epistimis.face.ui.tests/src-gen/.gitignore
+git restore com.epistimis.face.ui.tests/xtend-gen/.gitignore
 git restore com.epistimis.face.ui/src-gen/.gitignore
 git restore com.epistimis.face/src-gen/.gitignore
 git restore com.epistimis.face.web/src-gen/.gitignore
